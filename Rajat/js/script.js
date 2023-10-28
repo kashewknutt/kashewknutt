c43a0bda15abd79fe744d9b726fae4354a856617
Kashew Knutt <141485474+kashewknutt@users.noreply.github.com>
2023-10-28 10:20:37 +0530

    Add files via upload

---
+++ Rajat/js/script.js
@@ -0,0 +1,8 @@
+var aids = document.getElementById('aids');
+
+aids.onmouseover = function(){
+    document.getElementById('aidsHidden').style.display = 'block';
+}
+aids.onmouseout = function(){
+    document.getElementById('aidsHidden').style.display = 'none';
+}
