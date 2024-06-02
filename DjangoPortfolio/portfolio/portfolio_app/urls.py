ad09f91aeb6c07de2f55036c085b146bcbc6b0a9
Kashew Knutt <kashewknutt@gmail.com>
2024-06-02 23:46:27 +0530

    first webpage

--- DjangoPortfolio/portfolio/portfolio_app/urls.py
+++ DjangoPortfolio/portfolio/portfolio_app/urls.py
@@ -1,9 +1,6 @@
-# portfolio_app/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.project_list, name='project_list'),
-    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
-    # Define URLs for other views as needed
+    path('', views.home, name='home'),
 ]
--- DjangoPortfolio/portfolio/portfolio_app/views.py
+++ DjangoPortfolio/portfolio/portfolio_app/views.py
@@ -2,10 +2,5 @@
 from django.shortcuts import render
 from .models import Project
 
-def project_list(request):
-    projects = Project.objects.all()
-    return render(request, 'portfolio_app/project_list.html', {'projects': projects})
-
-def project_detail(request, pk):
-    project = Project.objects.get(pk=pk)
-    return render(request, 'portfolio_app/project_detail.html', {'project': project})
+def home(request):
+    return render(request, 'home.html')
