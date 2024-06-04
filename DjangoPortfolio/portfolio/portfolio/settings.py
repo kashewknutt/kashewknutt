7864157d7f01661a30e03ec6ab882dc104ba1f08
Kashew Knutt <kashewknutt@gmail.com>
2024-06-05 00:01:31 +0530

    picCommit

--- DjangoPortfolio/portfolio/portfolio/settings.py
+++ DjangoPortfolio/portfolio/portfolio/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- DjangoPortfolio/portfolio/portfolio_app/urls.py
+++ DjangoPortfolio/portfolio/portfolio_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.conf.urls.static import static
+from portfolio import settings
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('blogs/', views.blog, name='blog'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
